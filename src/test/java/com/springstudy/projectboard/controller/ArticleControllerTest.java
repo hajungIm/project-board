3f367e236ae00a56e0b17d7dbe7152b328c95e39
hajungIm <ihj_ewha@ewhain.net>
2023-06-23 15:08:34 +0900

    #16 - 컨트롤러 테스트 업데이트
    미디어 타입 검사 방법을 525c4b2 에서 확인했기 때문에,
    나머지 뷰도 동일하게 작동할 것으로 예상하고
    미리 테스트 메소드 업데이트

--- src/test/java/com/springstudy/projectboard/controller/ArticleControllerTest.java
+++ src/test/java/com/springstudy/projectboard/controller/ArticleControllerTest.java
@@ -46,7 +46,7 @@ class ArticleControllerTest {
         // When
         mvc.perform(get("/articles/1"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/detail"))
                 .andExpect(model().attributeExists("article"))
                 .andExpect(model().attributeExists("articleComments"));
@@ -64,7 +64,7 @@ class ArticleControllerTest {
         // When
         mvc.perform(get("/articles/search"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/search"));
 
         // Then
@@ -80,7 +80,7 @@ class ArticleControllerTest {
         // When
         mvc.perform(get("/articles/search-hashtag"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/search-hashtag"));
 
         // Then
