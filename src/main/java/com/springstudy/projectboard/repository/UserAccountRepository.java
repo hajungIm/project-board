1385ce3635301e3fbbe250dadc5fd3b61ae3ffd8
hajungIm <ihj_ewha@ewhain.net>
2023-06-26 21:10:14 +0900

    #19 - 회원 계정 도메인 구현
    'UserAccount' 이름으로 회원 계정 도메인 생성
    'user', 'account' 등은 mysql 예약어이므로 피함
    필드명도 mysql 예약어를 의식하여 정함

---
+++ src/main/java/com/springstudy/projectboard/repository/UserAccountRepository.java
@@ -0,0 +1,7 @@
+package com.springstudy.projectboard.repository;
+
+import com.springstudy.projectboard.domain.UserAccount;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserAccountRepository extends JpaRepository<UserAccount, Long> {
+}
