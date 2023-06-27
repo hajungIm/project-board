33dc5f1bf171357bc6f51bbfed7dba0cc959a448
hajungIm <ihj_ewha@ewhain.net>
2023-06-27 16:10:26 +0900

    #18 - 서비스 로직 테스트 코드 업데이트 및 DTO 재설계, 쿼리 메소드 작성
    서비스 로직 테스트 코드 추가 작성 및 보완
    연관된 DTO 재설계
    테스트가 필요로 하는 repository 쿼리 메소드 작성

--- src/main/java/com/springstudy/projectboard/dto/ArticleUpdateDto.java
+++
@@ -1,9 +0,0 @@
-package com.springstudy.projectboard.dto;
-
-public record ArticleUpdateDto(String title,
-                               String content,
-                               String hashtag){
-    public static ArticleUpdateDto of (String title, String content, String hashtag) {
-        return new ArticleUpdateDto(title, content, hashtag);
-    }
-}
--- src/main/java/com/springstudy/projectboard/service/ArticleService.java
+++ src/main/java/com/springstudy/projectboard/service/ArticleService.java
@@ -2,15 +2,13 @@ package com.springstudy.projectboard.service;
 
 import com.springstudy.projectboard.domain.type.SearchType;
 import com.springstudy.projectboard.dto.ArticleDto;
-import com.springstudy.projectboard.dto.ArticleUpdateDto;
+import com.springstudy.projectboard.dto.ArticleWithCommentsDto;
 import com.springstudy.projectboard.repository.ArticleRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.time.LocalDate;
-import java.util.List;
 
 @RequiredArgsConstructor
 @Transactional
@@ -20,12 +18,12 @@ public class ArticleService {
     private final ArticleRepository articleRepository;
 
     @Transactional(readOnly = true)
-    public Page<ArticleDto> searchArticles(SearchType title, String search_keyword) {
+    public Page<ArticleDto> searchArticles(SearchType title, String search_keyword, Pageable pageable) {
         return Page.empty();
     }
 
     @Transactional(readOnly = true)
-    public ArticleDto searchArticle(long l) {
+    public ArticleWithCommentsDto getArticle(long articleId) {
         return null;
     }
 
@@ -33,11 +31,13 @@ public class ArticleService {
 
     }
 
-    public void updateArticle(long articleId, ArticleUpdateDto dto) {
+    public void updateArticle(ArticleDto dto) {
 
     }
 
     public void deleteArticle(long articleId) {
 
     }
+
 }
+
