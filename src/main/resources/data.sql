80ecbe800234074c1b185bb5a70678c10479dac1
hajungIm <ihj_ewha@ewhain.net>
2023-06-27 12:06:40 +0900

    #21 - 회원 계정 엔티티와 게시글, 댓글에 연관관계 적용
    테이블 필드 수정으로 영향 받은
    'data.sql' 테스트 데이터도 모두 업데이트
    
    게시글 -> 댓글 컬렉션의 정렬을 최신 순으로 적용

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,12 +2,12 @@
 insert into user_account (user_id, user_password, nickname, email, memo, created_at, created_by, modified_at, modified_by)
 values ('ihj', 'abcd1234', 'IHJ', 'ihj@email.com', 'i am hj.', now(), 'ihj', now(), 'ihj');
 
-insert into article (title, content, hashtag, created_by, modified_by, created_at, modified_at)
-values ('abc', 'i am sue', '#purple', 'a', 'b', '2021-05-30 23:53:46', '2021-03-10 08:48:50');
+insert into article (user_account_id, title, content, hashtag, created_by, modified_by, created_at, modified_at)
+values (1, 'abc', 'i am sue', '#purple', 'a', 'b', '2021-05-30 23:53:46', '2021-03-10 08:48:50');
 
-insert into article_comment (content, created_at, created_by, modified_at, modified_by, article_id)
-values ('abcd', '2021-05-30 23:53:46', 'im', '2021-03-10 08:48:50', 'im', 1);
-insert into article_comment (content, created_at, created_by, modified_at, modified_by, article_id)
-values ('efgh', '2021-05-30 23:53:46', 'im', '2021-03-10 08:48:50', 'im', 1);
+insert into article_comment (article_id, user_account_id, content, created_at, created_by, modified_at, modified_by)
+values (1, 1, 'abcd', '2021-05-30 23:53:46', 'im', '2021-03-10 08:48:50', 'im');
+insert into article_comment (article_id, user_account_id, content, created_at, created_by, modified_at, modified_by)
+values (1, 1, 'efgh', '2021-05-30 23:53:46', 'im', '2021-03-10 08:48:50', 'im');
 
 
