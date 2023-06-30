3bd07b199cb91036c8ef9234b4466926417a3186
hajungIm <ihj_ewha@ewhain.net>
2023-06-30 17:14:23 +0900

    #30 - 해시태그 검색 페이지를 위한 기능 구현
    해시태그 뷰에 모든 게시글에서 나오는 해시태그를
    정리해서 보여주는 기능을 넣기 위해,
    해시태그만 뽑아서 unique 하게 보여주는 쿼리를
    querydsl 로 작성함
    그 외 서비스 코드, 컨트롤러, 관련 테스트 작성

--- src/main/java/com/springstudy/projectboard/controller/ArticleController.java
+++ src/main/java/com/springstudy/projectboard/controller/ArticleController.java
@@ -5,6 +5,7 @@ import com.springstudy.projectboard.dto.response.ArticleResponse;
 import com.springstudy.projectboard.dto.response.ArticleWithCommentsResponse;
 import com.springstudy.projectboard.service.ArticleService;
 import com.springstudy.projectboard.service.PaginationService;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -54,5 +55,22 @@ public class ArticleController {
         return "articles/detail";
     }
 
+    @GetMapping("/search-hashtag")
+    public String searchHashtag(
+            @RequestParam(required = false) String searchValue,
+            @PageableDefault(size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable,
+            ModelMap map
+    ) {
+        Page<ArticleResponse> articles = articleService.searchArticlesViaHashtag(searchValue, pageable).map(ArticleResponse::from);
+        List<Integer> barNumbers = paginationService.getPaginationBarNumbers(pageable.getPageNumber(), articles.getTotalPages());
+        List<String> hashtags = articleService.getHashtags();
+
+        map.addAttribute("articles", articles);
+        map.addAttribute("hashtags", hashtags);
+        map.addAttribute("paginationBarNumbers", barNumbers);
+        map.addAttribute("searchType", SearchType.HASHTAG);
+
+        return "articles/search-hashtag";
+    }
 
 }
---
+++ src/main/java/com/springstudy/projectboard/repository/querydsl/ArticleRepositoryCustom.java
@@ -0,0 +1,7 @@
+package com.springstudy.projectboard.repository.querydsl;
+
+import java.util.List;
+
+public interface ArticleRepositoryCustom {
+    List<String> findAllDistinctHashtags();
+}
