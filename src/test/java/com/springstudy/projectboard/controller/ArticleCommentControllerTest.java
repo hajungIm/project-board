ffe75ed9861dbee31047ad793a9e3e8647a12fac
hajungIm <ihj_ewha@ewhain.net>
2023-07-11 18:24:07 +0900

    #63 - 댓글 컨트롤러 테스트 업데이트: 대댓글 케이스 추가
    대댓글이 포함된 댓글 저장 api 의 입출력 테스트 추가

--- src/test/java/com/springstudy/projectboard/controller/ArticleCommentControllerTest.java
+++ src/test/java/com/springstudy/projectboard/controller/ArticleCommentControllerTest.java
@@ -33,8 +33,7 @@ class ArticleCommentControllerTest {
     private final MockMvc mvc;
     private final FormDataEncoder formDataEncoder;
 
-    @MockBean
-    private ArticleCommentService articleCommentService;
+    @MockBean private ArticleCommentService articleCommentService;
 
     public ArticleCommentControllerTest(
             @Autowired MockMvc mvc,
@@ -91,6 +90,30 @@ class ArticleCommentControllerTest {
 
         // Then
         then(articleCommentService).should().deleteArticleComment(articleCommentId, userId);
+    }
+
+    @WithUserDetails(value = "ihjTest", setupBefore = TestExecutionEvent.TEST_EXECUTION)
+    @DisplayName("[view][POST] 대댓글 등록 - 정상 호출")
+    @Test
+    void givenArticleCommentInfoWithParentCommentId_whenRequesting_thenSavesNewChildComment() throws Exception {
+        // Given
+        long articleId = 1L;
+        ArticleCommentRequest request = ArticleCommentRequest.of(articleId, 1L, "test comment");
+        willDoNothing().given(articleCommentService).saveArticleComment(any(ArticleCommentDto.class));
+
+        // When
+
+        // Then
+        mvc.perform(
+                        post("/comments/new")
+                                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                                .content(formDataEncoder.encode(request))
+                                .with(csrf())
+                )
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/articles/" + articleId))
+                .andExpect(redirectedUrl("/articles/" + articleId));
+        then(articleCommentService).should().saveArticleComment(any(ArticleCommentDto.class));
     }
 
 }
