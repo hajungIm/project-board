af325344e66be3283a9015e6e25217b21f446f6c
hajungIm <ihj_ewha@ewhain.net>
2023-07-11 18:23:01 +0900

    #63 - 대댓글 비즈니스 로직 구현
    부모/자식 관계를 만들면서 entity <-> dto 간 변환 로직,
    댓글 서비스에서 댓글과 대댓글을 저장하고 삭제하는 방법,
    댓글과 대댓글을 db 데이터로부터 변환할 때
    서로 계층 구조를 만들고 각각 정렬하는 과정을 구현

--- src/main/java/com/springstudy/projectboard/dto/response/ArticleCommentResponse.java
+++ src/main/java/com/springstudy/projectboard/dto/response/ArticleCommentResponse.java
@@ -3,6 +3,9 @@ package com.springstudy.projectboard.dto.response;
 import com.springstudy.projectboard.dto.ArticleCommentDto;
 
 import java.time.LocalDateTime;
+import java.util.Comparator;
+import java.util.Set;
+import java.util.TreeSet;
 
 public record ArticleCommentResponse(
         Long id,
@@ -10,11 +13,20 @@ public record ArticleCommentResponse(
         LocalDateTime createdAt,
         String email,
         String nickname,
-        String userId
+        String userId,
+        Long parentCommentId,
+        Set<ArticleCommentResponse> childComments
 ) {
 
     public static ArticleCommentResponse of(Long id, String content, LocalDateTime createdAt, String email, String nickname, String userId) {
-        return new ArticleCommentResponse(id, content, createdAt, email, nickname, userId);
+        return ArticleCommentResponse.of(id, content, createdAt, email, nickname, userId, null);
+    }
+
+    public static ArticleCommentResponse of(Long id, String content, LocalDateTime createdAt, String email, String nickname, String userId, Long parentCommentId) {
+        Comparator<ArticleCommentResponse> childCommentComparator = Comparator
+                .comparing(ArticleCommentResponse::createdAt)
+                .thenComparingLong(ArticleCommentResponse::id);
+        return new ArticleCommentResponse(id, content, createdAt, email, nickname, userId, parentCommentId, new TreeSet<>(childCommentComparator));
     }
 
     public static ArticleCommentResponse from(ArticleCommentDto dto) {
@@ -29,7 +41,12 @@ public record ArticleCommentResponse(
                 dto.createdAt(),
                 dto.userAccountDto().email(),
                 nickname,
-                dto.userAccountDto().userId()
+                dto.userAccountDto().userId(),
+                dto.parentCommentId()
         );
+    }
+
+    public boolean hasParentComment() {
+        return parentCommentId != null;
     }
 }
